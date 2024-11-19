539958121197bb9813f568ab623954b5b603267e
andyhaz <andyhaz1970@gmail.com>
2024-11-19 17:00:54 -0600

    Initial Commit

---
+++ swiftui-binabble/swiftui_binabbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_binabbleApp.swift
+//  swiftui-binabble
+//
+//  Created by andy Hazlett on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_binabbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
